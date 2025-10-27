5b820dd0509d4d45eeab9ff5cb3dad7b81b33b4e
riounjanegalura-pixel <riounjanegalura@gmail.com>
2025-10-27 18:47:47 +0800

    add code in button

--- 05_Laboratory_Excercise/FrmOpenTextFile.cs
+++ 05_Laboratory_Excercise/FrmOpenTextFile.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,7 +20,32 @@ namespace _05_Laboratory_Excercise
 
         private void bntOpen_Click(object sender, EventArgs e)
         {
+            openFileDialog1.InitialDirectory = @"C:\";
+            openFileDialog1.Title = "Browse Text Files";
+            openFileDialog1.DefaultExt = "txt";
+            openFileDialog1.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";         
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string path = openFileDialog1.FileName;
+                lvShowText.Items.Clear();
 
+                try
+                {
+                    
+                    string[] lines = System.IO.File.ReadAllLines(path);
+                    foreach (string line in lines)
+                    {
+                        Console.WriteLine(line);
+                        lvShowText.Items.Add(line);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("An error occurred: " + ex.Message);
+                }
+            }
         }
     }
 }
+
+
